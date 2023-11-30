a903cc95cedbcab9ef05981db506ab6a919a6d45
Red <95549175+TheRedSS@users.noreply.github.com>
2023-11-30 13:55:02 +0300

    products.sql

--- products.sql
+++ products.sql
@@ -1,31 +1,12 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
---
--- Anamakine: 127.0.0.1
--- Üretim Zamanı: 30 Kas 2023, 11:50:09
 -- Sunucu sürümü: 10.4.28-MariaDB
 -- PHP Sürümü: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Veritabanı: `alisveris.urunler.json`
---
-
--- --------------------------------------------------------
-
---
--- Tablo için tablo yapısı `products`
---
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
@@ -35,7 +16,7 @@ CREATE TABLE `products` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Tablo döküm verisi `products`
+--`products`
 --
 
 INSERT INTO `products` (`id`, `urunAd`, `stok`, `resimUrl`) VALUES
@@ -69,22 +50,14 @@ INSERT INTO `products` (`id`, `urunAd`, `stok`, `resimUrl`) VALUES
 (28, 'Zımba', 100, '');
 
 --
--- Dökümü yapılmış tablolar için indeksler
+-- index's
 --
 
 --
--- Tablo için indeksler `products`
+-- in table index `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
-
---
--- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
---
-
---
--- Tablo için AUTO_INCREMENT değeri `products`
---
 ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
