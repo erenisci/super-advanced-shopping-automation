56745330ea9232062455a4bf95f456ba7b0291f7
Eren İşci <isci.eren@gmail.com>
2023-12-16 22:01:22 +0300

    3

--- content/urunler1.sql
+++
@@ -1,9 +0,0 @@
-USE projectdb;
- 
-CREATE TABLE IF NOT EXISTS kategoriler (
-    urunKategori_id INT AUTO_INCREMENT PRIMARY KEY,
-    urunKategori_ad VARCHAR(255) NOT NULL
-) ENGINE=INNODB;
-
-INSERT INTO kategoriler (urunKategori_ad) VALUES
- ('Mutfak'), ('Teknoloji'), ('Kırtasiye'), ('Eşya'), ('Ev'), ('Giyim'), ('Yemek');
